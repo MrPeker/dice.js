d0dcdde6d99938292612684a73071b1ad5a86821
Mehmet Ali Peker <maps6134@hotmail.com>
2020-06-18 17:17:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 module.exports = {
   dice: () => (Math.floor(Math.random() * 6) + 1),
-  random: (max, min = 0) => (Math.floor(Math.random() * max) + min)
-}
+  random: (max, min = 0) => (Math.floor(Math.random() * (max - min)) + min),
+  quantumDice: () => new Array(Math.floor(Math.random() * 3)).fill(null).map(() => ((Math.random() * 6) + 1)),
+  quantumRandom: (max, min = 0) => new Array(Math.floor(Math.random() * 3)).fill(null).map(() => ((Math.random() * (max - min)) + min)),
+};
