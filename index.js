5ad246b19ca91c5ff653f64f0ea2a8fb31b90480
Mehmet Ali Peker <maps6134@hotmail.com>
2020-07-08 02:28:17 +0300

    test

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ module.exports = {
   quantumDice: () => new Array(Math.floor(Math.random() * 3)).fill(null).map(() => ((Math.random() * 6) + 1)),
   quantumRandom: (max, min = 0) => new Array(Math.floor(Math.random() * 3)).fill(null).map(() => ((Math.random() * (max - min)) + min)),
 };
+
+//
